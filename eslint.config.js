1946ed56a6911cd46c96fe2535c8ab592a9f6aab
west-mushroom <3091886048@qq.com>
2024-12-26 18:32:56 +0800

    引入静态资源，图标，主体页面布局等

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,17 @@ export default [
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
+  {
+  // 自定义规则
+  rules: {
+    // 禁用Vue组件命名规则（具体规则名称可能因eslint-plugin-vue版本而异）
+    'vue/multi-word-component-names': '0', // 允许单字组件名
+    // 其他自定义规则...
+  },
+  }
+ ,
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+
+
 ]
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
+
+
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+ <template>
+  <!--一级路由出口-->
+  <RouterView/>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
